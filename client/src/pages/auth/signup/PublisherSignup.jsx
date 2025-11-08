566fa0d144bc1f26a1b3f35f11abcdc67f979557
rithishreddy17 <samarithishreddy2005@gmail.com>
2025-11-08 23:38:54 +0530

    "started publisher Signup"

---
+++ client/src/pages/auth/signup/PublisherSignup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PublisherSignup = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-b from-purple-50 to-white bg-gray-50">
+      <div className="max-w-md w-full">
+      </div>
+    </div>
+  );
+};
+
+export default PublisherSignup;
